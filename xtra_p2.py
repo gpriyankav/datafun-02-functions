6fa6c61545c60a81db1ec9e6321490b8b4f3b7c4
gpriyankav <kongarap@hotmail.com>
2023-01-21 23:04:22 -0500

    Priyanka Gorentla Changes

--- xtra_p2.py
+++ xtra_p2.py
@@ -30,25 +30,34 @@ import doctest
 # define some existing functions
 def add_two(first, second):
     """Return the sum of any two arguments."""
-    sum = first   # fix this line
+    sum = first + second  # fix this line
     return sum
+
+print(f"Sum of 2 numbers: {add_two(1,2)}")    
+print("Addind 2 words:", (add_two("Vijay","Priyanka")))
+print()
 
 
 def add_triangle_list(list_triangle):
     """Return the sum of three numbers in a list."""
     sum = 0
     for value in list_triangle:
-        sum = sum + 0  # fix this line to add the value instead of 0
+        sum += value  # fix this line to add the value instead of 0
     return sum
+
+print(f"sum of three numbers: {add_triangle_list([3,4,5])}")
+print()
 
 
 def add_any(*args):
     """Return the sum of numbers, using built-in *args."""
     sum = 0
     for x in args:
-        sum += 1  # fix this line to add x instead of 1
+        sum += x  # fix this line to add x instead of 1
     return sum
 
+print(f"Sum of numbers using args: {add_any(1,1,1,1,1,1,1,1)}")
+print()
 
 def add_any_with_keywords(**kwargs):
     """Return the sum of numbers, using built-in keyword args, **kwargs."""
@@ -57,18 +66,20 @@ def add_any_with_keywords(**kwargs):
         sum += value  # Use the popular and concise version of sum = sum + x
     return sum
 
+print(f"Sum of Numbers: {add_any_with_keywords(a=1,b=2)}")
+print()
+
 
 # TODO: implment a new function to convert celsius to fahrenheit
 # Use round as needed to make the test pass
 # The name of the function is provided in the docstring above
 
-
-
-
-
-
-
-
+def convert_ctof(celtemp):
+    fahtemp = round(((celtemp*1.8)+32),2)
+    return fahtemp
+ 
+print(f"Celsius to Fahrenheit conversion: {convert_ctof(0)}")
+print(f"Celsius to Fahrenheit conversion: {convert_ctof(100)}")
 
 
 if __name__ == "__main__":
---
+++ xtra_p2out.py
@@ -0,0 +1,33 @@
+"""
+PS C:\Users\KONGA\Documents\datafun-02-functions> & C:/Users/KONGA/AppData/Local/Programs/Python/Python311/python.exe c:/Users/KON
+GA/Documents/datafun-02-functions/xtra_p2.py
+Sum of 2 numbers: 3
+Addind 2 words: VijayPriyanka
+
+sum of three numbers: 12
+
+Sum of numbers using args: 8
+
+Sum of Numbers: 3
+
+Celsius to Fahrenheit conversion: 32.0
+Celsius to Fahrenheit conversion: 212.0
+===========================================================
+Running doctest.testmod() function to unit test our code
+===========================================================
+
+
+===========================================================
+If you don't see any results, all tests passed.
+===========================================================
+Run with the -v flag to show results regardless.
+Hit up arrow (to get last command) and add space -v
+===========================================================
+
+Read error messages carefully.
+Errors tell which line number has the error
+and what the issue is. Scroll up in the terminal to see.
+Fix issues one at a time until you get the behavior
+Described in the docstring.
+
+"""
